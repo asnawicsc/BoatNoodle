17e368007e82838cbd4ec2ee8ba62d9f32ac6e4c
damien <yithanglee@gmail.com>
2018-05-30 23:17:35 +0800

    merge with food items

--- config/prod.exs
+++ config/prod.exs
@@ -8,4 +8,13 @@ config :logger, level: :info
 
 config :boat_noodle, BoatNoodleWeb.Endpoint, server: true
 
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+config :boat_noodle, BoatNoodle.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  hostname: "110.4.42.47",
+  port: "15100",
+  username: "phoenix_bn",
+  password: "123123",
+  database: "posgb_boatnoodle",
+  pool_size: 10,
+  timeout: 90000
