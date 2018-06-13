169895cedfb7b979d4d4acd138ff9ad5e20aec56
Asnawi <asnawicsc@gmail.com>
2018-06-13 18:19:10 +0800

    discount

--- lib/boat_noodle_web/controllers/discount_controller.ex
+++ lib/boat_noodle_web/controllers/discount_controller.ex
@@ -3,10 +3,58 @@ defmodule BoatNoodleWeb.DiscountController do
 
   alias BoatNoodle.BN
   alias BoatNoodle.BN.Discount
+  alias BoatNoodle.BN.DiscountItem
+  alias BoatNoodle.BN.ItemCat
+  alias BoatNoodle.BN.ItemSubcat
+
 
   def index(conn, _params) do
     discount = BN.list_discount()
-    render(conn, "index.html", discount: discount)
+
+    discount_details =
+      Repo.all(
+        from(
+          s in Discount,
+          select: %{
+            discountid: s.discountid,
+            discount_name: s.discname,
+            discount_description: s.descriptions,
+            activate: s.disc_qty
+          }
+        )
+      )
+
+       discount_items =
+      Repo.all(
+        from(
+          s in DiscountItem,
+          left_join: b in Discount, where: b.discountid==s.discountid,
+          left_join: c in ItemCat, where: s.target_cat==c.itemcatid,
+          left_join: d in ItemSubcat, where: s.is_targetmenuitems==d.subcatid,
+          select: %{
+            discountitemsid: s.discountitemsid,
+            discitemsname: s.discitemsname,
+            description: s.descriptions,
+            discamtpercentage: s.discamtpercentage,
+            discountcategory: b.discname,
+            target_menu_category: c.itemcatname,
+            target_menu_item: d.itemname,
+            activate: s.disc_qty
+          }
+        )
+      )
+
+      discount_catalog =
+      Repo.all(
+        from(
+          s in DiscountCatalog,
+          select: %{
+            id: s.id,
+            name: s.name
+          }
+        )
+      )
+    render(conn, "index.html",discount_catalog: discount_catalog,discount_items: discount_items,discount_details: discount_details, discount: discount)
   end
 
   def new(conn, _params) do
