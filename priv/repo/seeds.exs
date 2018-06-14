b68efd1ff335a08e84e4fb25020820a992e66ebf
Damien Lee <yithanglee@gmail.com>
2018-06-14 16:48:05 +0800

    include sql for brand id

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -23,4 +23,88 @@ ALTER TABLE `posgb_boatnoodle`.`tag_tbl`
 ADD COLUMN `combo_item_ids` LONGTEXT NULL DEFAULT NULL AFTER `printer_IP`;
 
 
+CREATE TABLE `posgb_boatnoodle`.`brand` (
+  `brand_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`brand_id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
+
+ALTER TABLE `posgb_boatnoodle`.`brand` 
+ADD COLUMN `domain_name` VARCHAR(45) NOT NULL AFTER `name`,
+ADD UNIQUE INDEX `domain_name_UNIQUE` (`domain_name` ASC);
+
+ALTER TABLE `posgb_boatnoodle`.`branch` 
+ADD COLUMN `brand_ID` INT NOT NULL DEFAULT 1 AFTER `created_at`;
+
+ALTER TABLE `posgb_boatnoodle`.`branch` 
+ADD INDEX `brand_id` (`brand_ID` ASC);
+ALTER TABLE `posgb_boatnoodle`.`branch` 
+ADD CONSTRAINT `fk_branch_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+
+ALTER TABLE `posgb_boatnoodle`.`combo_details` 
+ADD COLUMN `brand_ID` INT NOT NULL DEFAULT 1;
+
+ALTER TABLE `posgb_boatnoodle`.`combo_details` 
+ADD INDEX `combo_detail_brand_idx` (`brand_ID` ASC);
+ALTER TABLE `posgb_boatnoodle`.`combo_details` 
+ADD CONSTRAINT `combo_detail_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `posgb_boatnoodle`.`discount` 
+ADD COLUMN `brand_ID` INT(11) NOT NULL DEFAULT '1' ;
+
+ALTER TABLE `posgb_boatnoodle`.`discount` 
+ADD CONSTRAINT `disc_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `posgb_boatnoodle`.`discount_catalog` 
+ADD COLUMN `brand_ID` INT(11) NOT NULL DEFAULT '1';
+
+ALTER TABLE `posgb_boatnoodle`.`discount_catalog` 
+ADD CONSTRAINT `disc_cat_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `posgb_boatnoodle`.`discountitems` 
+ADD COLUMN `brand_ID` INT(11) NOT NULL DEFAULT '1';
+
+ALTER TABLE `posgb_boatnoodle`.`discountitems` 
+ADD CONSTRAINT `disc_item_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `posgb_boatnoodle`.`item_cat` 
+ADD COLUMN `brand_ID` INT(11) NOT NULL DEFAULT '1';
+
+ALTER TABLE `posgb_boatnoodle`.`item_cat` 
+ADD CONSTRAINT `item_cat_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `posgb_boatnoodle`.`itemsremak` 
+ADD COLUMN `brand_ID` INT(11) NOT NULL DEFAULT '1';
+
+ALTER TABLE `posgb_boatnoodle`.`itemsremak` 
+ADD CONSTRAINT `itemsremak_brand`
+  FOREIGN KEY (`brand_ID`)
+  REFERENCES `posgb_boatnoodle`.`brand` (`brand_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 "
